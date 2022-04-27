aaeb2b402312cc2af64d9b0f6756b2ffb006477b
Elena María Pérez Pérez <elena.perez.perez@alumnos.upm.es>
2022-04-27 11:19:54 +0200

    Event view

--- lib/ui/pages/routing.dart
+++ lib/ui/pages/routing.dart
@@ -7,11 +7,15 @@ import 'package:barriolympics/ui/pages/statistics_page.dart';
 import 'package:barriolympics/utils.dart';
 import 'package:flutter/material.dart';
 
+import 'events/view_event_page.dart';
+
 const String POST_IMAGE_VIEW = "postImageView";
 const String STATISTICS_VIEW = "statisticsView";
 const String EVENT_LIST_VIEW_ROUTE = "eventListView";
 const String EDIT_EVENT_PAGE = "editEventPage";
 const String NEW_EVENT_PAGE = "newEventPage";
+const String VIEW_EVENT_PAGE = "viewEventPage";
+
 
 PageRoute? checkGeneralRoutes(RouteSettings route) {
   switch (route.name) {
@@ -19,7 +23,6 @@ PageRoute? checkGeneralRoutes(RouteSettings route) {
       return PageRouteBuilder(
         pageBuilder: (BuildContext context, _, __) {
           Post? post = getArgument(route, "post");
-
           return ImageView(
             post: post,
             index: 0,
@@ -39,6 +42,11 @@ PageRoute? checkGeneralRoutes(RouteSettings route) {
       return PageRouteBuilder(pageBuilder: (BuildContext context, _, __) {
         Event event = getArgument(route, "event");
         return EditEventPage(event: event);
+      });
+    case VIEW_EVENT_PAGE:
+      return PageRouteBuilder(pageBuilder: (BuildContext context, _, __) {
+        Event event = getArgument(route, "event");
+        return ViewEventPage(event: event);
       });
   }
   return null;

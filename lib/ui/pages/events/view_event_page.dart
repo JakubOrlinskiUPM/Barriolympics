45dd1e5f4f3090b1ada98190de3c1f05933fe2ee
Elena María Pérez Pérez <elena.perez.perez@alumnos.upm.es>
2022-04-27 11:30:52 +0200

    event view

---
+++ lib/ui/pages/events/view_event_page.dart
@@ -0,0 +1,58 @@
+import 'package:barriolympics/models/event.dart';
+import 'package:barriolympics/provider/app_state.dart';
+import 'package:barriolympics/ui/pages/new_event/edit_event_step.dart';
+import 'package:barriolympics/ui/pages/new_event/new_event_splash_screen.dart';
+import 'package:barriolympics/ui/pages/new_event/edit_event_step_basic_info.dart';
+import 'package:barriolympics/ui/pages/new_event/edit_event_step_loc.dart';
+import 'package:barriolympics/ui/pages/new_event/edit_event_step_permits.dart';
+import 'package:barriolympics/ui/pages/new_event/edit_event_step_publish.dart';
+import 'package:flutter/material.dart';
+import 'package:im_stepper/stepper.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+import '../../../models/barrio.dart';
+
+class ViewEventPage extends StatefulWidget {
+  ViewEventPage({Key? key, required this.event}) : super(key: key);
+
+  final Event event;
+
+  @override
+  State<ViewEventPage> createState() => _ViewEventPageState();
+}
+
+class _ViewEventPageState extends State<ViewEventPage> {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _dateController = TextEditingController(
+      text: widget.event.date != null
+          ? DateFormat('yyyy-MM-dd').format(widget.event.date!)
+          : null,
+    );
+    TextEditingController _timeController = TextEditingController(
+      text:
+          widget.event.time != null ? widget.event.time!.format(context) : null,
+    );
+    TextEditingController _descriptionController = TextEditingController(
+      text: widget.event.description,
+    );
+
+    AppState appState = Provider.of<AppState>(context, listen: false);
+
+    return Column(
+      children: [
+        Image.network(widget.event.fileUrl.toString()),
+        Column(children: [
+          Text(
+              widget.event.name.toString()
+          ),
+          Text(widget.event.barrio.toString()), //barrio??? is not in the dummy data -> ==null
+          Text(widget.event.date.toString().substring(0, 10)),
+          Text(widget.event.time.toString().substring(10, 15)),
+          Text(widget.event.description.toString()),
+        ])
+      ],
+    );
+  }
+}
